7f566c810396f697edbb95c4306a8095f69ad620
katsu0212 <katsu02122728@gmail.com>
2021-12-11 12:08:02 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,57 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:edit, :update]
+
   def index
+      @room_lists = Room.all
+      @room_joining = RoomUser.where(user_id: current_user.id)
+      @room_lists_none = "グループに参加していません。"
   end
-  
+
   def new
-    @room = Room.new
+      @room = Room.new
+      @room.users << current_user
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to rooms_path, notice: 'グループを作成しました。'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to rooms_path, notice: 'グループを更新しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+      delete_room = Room.find(params[:id])
+      if delete_room.destroy
+          redirect_to rooms_path, notice: 'グループを削除しました。'
+      end
+  end
+
+  private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "posts#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:index, :new, :create ]
+  resources :rooms
 end
